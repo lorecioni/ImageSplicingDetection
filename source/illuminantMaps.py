9e0ad7f2fc383b19a89afbb4200eac0b755eddcc
Lorenzo Cioni <lore.cioni@gmail.com>
2016-10-04 13:28:31 +0200

    illuminant maps

--- source/illuminantMaps.py
+++ source/illuminantMaps.py
@@ -8,13 +8,40 @@ import sys
 import os
 from subprocess import *
 
-#IIC configuration file
+#Segment image
+
+max_intensity = 0.98823529411764705882
+min_intensity = .05882352941176470588
+
+'''Preparing image for illuminant methods'''
+def prepareImageIlluminants(img, sigma, k, min_size, max_intensity, min_intensity):
+    print('Segmenting image for illuminant methods...')
+    filename = img.split('/')
+    filename = filename[len(filename) - 1]
+    command = "illuminants/build/./vole felzenszwalb -I " + img + " --deterministic_coloring -O maps/" + filename[:-4] + "_segmented.png --sigma " + str(sigma) + " --k " + str(k) + " --min_size " + str(min_size) + " --max_intensity " + str(max_intensity) + " --min_intensity " + str(min_intensity)
+    os.system(command)
+    #print(command)
+    print('Image segmented')
+    
+    
+''' Extracting GGE illuminant map'''
+def extractGGEMap(img, segmentedImg, sigma, n, p):
+    print('Extracting GGE map...')
+    filename = img.split('/')
+    filename = filename[len(filename) - 1]
+    command = "illum_maps/build/./vole lgrayworld --img.image " + img + " -S " + "maps/" + filename[:-4] + "_gge_map.png --n " +  str(n) + " --p " + str(p) + " --sigma " + str(sigma)
+    os.system(command)
+    print('GGE map extracted')
+
+    
+''' Extracting IIC illuminant map'''
 config = "config.txt"
 
 def extractIICMap(img, segmentedImg):
     print('Extracting IIC map...')
     filename = img.split('/')
     filename = filename[len(filename) - 1]
-    command = "illum_maps/build/./vole liebv --img.image " + img + " -S " + segmentedImg + " -O maps/"  + filename[:-4] + "_fhs.png --iebv_config illum_maps/build/" + config
+    command = "illuminants/build/./vole liebv --img.image " + img + " -S maps/" + segmentedImg + " -O maps/"  + filename[:-4] + "_iic_map.png --iebv_config illuminants/build/" + config
     os.system(command)
+    #print(command)
     print('IIC map extracted')
--- source/splicingDetection.py
+++ source/splicingDetection.py
@@ -4,8 +4,36 @@ Created on 03 ott 2016
 @author: lorenzocioni
 '''
 
+import illuminantMaps
+
+max_intensity = 0.98823529411764705882
+min_intensity = .05882352941176470588
+
+
 def detectSplice(img):
-    # 1. Extracting GGE and IIC illuminant maps
+    ''' 1. Extracting GGE and IIC illuminant maps '''
+    filename = img.split('/')
+    filename = filename[len(filename) - 1]
+    filename = filename[:-4]
+    
+    ''' 1.1 Preparing image for illuminant methods '''
+    illuminantMaps.prepareImageIlluminants(img, 0.2, 300, 15, min_intensity, max_intensity)
+    
+    ''' 1.2 Extracting IIC illuminant map '''
+    illuminantMaps.extractIICMap(img, filename + "_segmented.png")
+    
+    # 2. Statistical difference between IIC and GGE maps
+    
+    # 2.1 Building heat maps (only for visualizations)
+    
+    # 2.2 Evaluate statistical difference
+    
+    # 2.2.1 Use a set of different metrics
+    
+    # 3 ROI descriptors
+    
+    # 4 Classifications
+    
     return
     
     
