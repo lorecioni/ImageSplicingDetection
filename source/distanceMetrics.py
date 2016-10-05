7072311a9689d7c6c40a9165515dd55d844c55c0
Lorenzo Cioni <lore.cioni@gmail.com>
2016-10-05 13:06:32 +0200

    distance metrics

--- source/distanceMetrics.py
+++ source/distanceMetrics.py
@@ -2,5 +2,23 @@
 Created on 05 ott 2016
 
 @author: lorenzocioni
-'''
 
+Different matrics for evaluating distances bewteen eigenvalues
+'''
+import math
+
+def linearDistance(a, b):
+    return a - b
+
+def quadraticDistance(a, b):
+    return pow((a - b), 2)
+
+def logarithmicDistance(a, b):
+    return math.log(abs(a - b))
+
+def squareRootDistance(a, b):
+    return math.sqrt(abs(a - b))
+
+def cubicRootDistance(a, b):
+    return pow((a - b), 1/3)
+    
