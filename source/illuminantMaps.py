3dec6823a0ca110ec70686d71dcfb2e649ba38c6
Lorenzo Cioni <lore.cioni@gmail.com>
2016-10-07 10:31:00 +0200

    suppress output

--- source/illuminantMaps.py
+++ source/illuminantMaps.py
@@ -12,6 +12,7 @@ from subprocess import *
 
 max_intensity = 0.98823529411764705882
 min_intensity = .05882352941176470588
+devnull = open(os.devnull, 'w')
 
 #Preparing image for illuminant methods
 def prepareImageIlluminants(img, sigma, k, min_size, max_intensity, min_intensity, verbose):
@@ -19,10 +20,9 @@ def prepareImageIlluminants(img, sigma, k, min_size, max_intensity, min_intensit
         print('Segmenting image for illuminant methods...')
     filename = img.split('/')
     filename = filename[len(filename) - 1]
-    command = "illuminants/build/./vole felzenszwalb -I " + img + " --deterministic_coloring -O maps/" + filename[:-4] + "_segmented.png --sigma " + str(sigma) + " --k " + str(k) + " --min_size " + str(min_size) + " --max_intensity " + str(max_intensity) + " --min_intensity " + str(min_intensity)
-    os.system(command)
+    command = ["illuminants/build/./vole", "felzenszwalb", "-I " + img, "--deterministic_coloring", "-O maps/" + filename[:-4] + "_segmented.png", "--sigma " + str(sigma),  "--k " + str(k), "--min_size " + str(min_size), "--max_intensity " + str(max_intensity), "--min_intensity " + str(min_intensity)]
+    call(command, stdout = devnull, stderr = devnull, shell = True)
     if verbose:
-        print(command)
         print('Image segmented')
     
     
@@ -32,8 +32,8 @@ def extractGGEMap(img, segmentedImg, sigma, n, p, verbose):
         print('Extracting GGE map...')
     filename = img.split('/')
     filename = filename[len(filename) - 1]
-    command = "illuminants/build/./vole lgrayworld --img.image " + img + " -S maps/" + segmentedImg + " -O maps/" + filename[:-4] + "_gge_map.png --n " +  str(n) + " --p " + str(p) + " --sigma " + str(sigma)
-    os.system(command)
+    command = ["illuminants/build/./vole", "lgrayworld", "--img.image " + img, "-S maps/" + segmentedImg, "-O maps/" + filename[:-4] + "_gge_map.png",  "--n " +  str(n), "--p " + str(p), "--sigma " + str(sigma)]
+    call(command, stdout = devnull, stderr = devnull, shell = True)
     if verbose:
         print('GGE map extracted')
 
@@ -46,8 +46,8 @@ def extractIICMap(img, segmentedImg, verbose):
         print('Extracting IIC map...')
     filename = img.split('/')
     filename = filename[len(filename) - 1]
-    command = "illuminants/build/./vole liebv --img.image " + img + " -S maps/" + segmentedImg + " -O maps/"  + filename[:-4] + "_iic_map.png --iebv_config illuminants/build/" + config
-    os.system(command)
+    command = ["illuminants/build/./vole", "liebv", "--img.image " + img, "-S maps/" + segmentedImg, "-O maps/"  + filename[:-4] + "_iic_map.png", "--iebv_config illuminants/build/" + config]
+    call(command, stdout = devnull, stderr = devnull, shell = True)
     if verbose:
         print('IIC map extracted')
 
